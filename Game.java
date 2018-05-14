7a4ea81eec03fa0e23e3ddd5ae7491ba47575d83
OstapFilipenko <filipenkos_13@mail.ru>
2018-05-14 16:34:50 +0200

    MineSweeoer v.5

--- Game.java
+++ Game.java
@@ -8,20 +8,20 @@ public class Game {
 	static String[][] spielfeld = spielfeldAnlegen(_zeilen, _spalten, false);
 	static int mines = 0;
 	static int versuche = 0;
-	
+
 	public static void main(String[] args) {
 		boolean ende = false;
 		while (ende == false) {
 			spielfeldAnzeigen();
-			int posZeile = eingeben("Bitte Zeile wählen");
-			int posSpalte = eingeben("Bitte Spalte wählen");
+			int posZeile = eingeben("Bitte Zeile wählen", (_zeilen-1));
+			int posSpalte = eingeben("Bitte Spalte wählen", (_spalten-1));
 			ende = spielfeldPruefen(posZeile, posSpalte);
 		}
 	}
-	
+
 	public static boolean spielfeldPruefen(int zeile, int spalte) {
 		if (minenfeld[zeile][spalte] == "[x]") {
-			spielfeld[zeile][spalte] = "[X]";
+			spielfeld[zeile][spalte] = "<X>";
 			spielfeldAnzeigen();
 			System.out.println("Bummm\nDu hast leider die Mine erwischt...");
 			System.out.println("Game over!");
@@ -31,12 +31,22 @@ public class Game {
 			return false;
 		}
 	}
-	
-	public static int eingeben(String hinweis) {
+
+	public static int eingeben(String hinweis, int max) {
+
 		String eingabe = JOptionPane.showInputDialog(hinweis);
-		return Integer.parseInt(eingabe);
+		int zahl = Integer.parseInt(eingabe);
+		
+		if (zahl > max || zahl < 0) {
+			JOptionPane.showMessageDialog(null, "!!!Number between 0 and " + max + "!!!");
+			String eingabe1 = JOptionPane.showInputDialog(hinweis);
+			zahl = Integer.parseInt(eingabe1);
+		}
+		
+		return zahl;
+
 	}
-	
+
 	public static void spielfeldAnzeigen() {
 		for (int z = 0; z < _zeilen; z++) {
 			for (int s = 0; s < _spalten; s++) {
@@ -46,7 +56,7 @@ public class Game {
 		}
 		System.out.println("---------");
 	}
-	
+
 	public static String[][] spielfeldAnlegen(int zeilen, int spalten, boolean mienenAnlegen) {
 		String[][] mineField = new String[zeilen][spalten];
 		for (int z = 0; z < zeilen; z++) {
@@ -55,13 +65,12 @@ public class Game {
 			}
 		}
 		if (mienenAnlegen) {
-			mineField[(zeilen-1)][0] = "[x]";
-			mineField[(zeilen-1)][(spalten-1)] = "[x]";
-			mineField[0][(spalten-1)] = "[x]";
+			mineField[(zeilen - 1)][0] = "[x]";
+			mineField[(zeilen - 1)][(spalten - 1)] = "[x]";
+			mineField[0][(spalten - 1)] = "[x]";
 			mines = 3;
 		}
 		return mineField;
 	}
-	
-	
+
 }
