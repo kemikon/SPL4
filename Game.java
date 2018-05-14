ff309da3f09d418dd3d0f6e2d56696e5baa67cf5
OstapFilipenko <filipenkos_13@mail.ru>
2018-05-14 15:54:21 +0200

    MineSweeper v.4

--- Game.java
+++ Game.java
@@ -1,12 +1,14 @@
 import javax.swing.JOptionPane;
 
 public class Game {
+
 	static int _zeilen = 3;
 	static int _spalten = 3;
 	static String[][] minenfeld = spielfeldAnlegen(_zeilen, _spalten, true);
 	static String[][] spielfeld = spielfeldAnlegen(_zeilen, _spalten, false);
 	static int mines = 0;
-
+	static int versuche = 0;
+	
 	public static void main(String[] args) {
 		boolean ende = false;
 		while (ende == false) {
@@ -16,49 +18,50 @@ public class Game {
 			ende = spielfeldPruefen(posZeile, posSpalte);
 		}
 	}
-
+	
 	public static boolean spielfeldPruefen(int zeile, int spalte) {
 		if (minenfeld[zeile][spalte] == "[x]") {
-			System.out.println("Bummm - du hast leider die Mine erwischt");
+			spielfeld[zeile][spalte] = "[X]";
+			spielfeldAnzeigen();
+			System.out.println("Bummm\nDu hast leider die Mine erwischt...");
 			System.out.println("Game over!");
 			return true;
 		} else {
 			spielfeld[zeile][spalte] = "[*]";
+			return false;
 		}
-		return false;
 	}
-
+	
 	public static int eingeben(String hinweis) {
 		String eingabe = JOptionPane.showInputDialog(hinweis);
 		return Integer.parseInt(eingabe);
 	}
-
-	public static String[][] spielfeldAnlegen(int Zeilen, int Spalten, boolean m) {
-		String[][] mineFeld = new String[Zeilen][Spalten];
-		for (int i = 0; i < mineFeld.length; i++) {
-			for (int q = 0; q < mineFeld.length; q++) {
-				mineFeld[i][q] = "[ ]";
-			}
-		}
-		if (m == true) {
-
-			mineFeld[2][0] = "[x]";
-			mineFeld[1][1] = "[x]";
-			mineFeld[0][2] = "[x]";
-			mines = 3;
-
-		}
-		return mineFeld;
-	}
-
+	
 	public static void spielfeldAnzeigen() {
-
-		for (int i = 0; i < _zeilen; i++) {
-			for (int q = 0; q < _spalten; q++) {
-				System.out.print(spielfeld[i][q]);
+		for (int z = 0; z < _zeilen; z++) {
+			for (int s = 0; s < _spalten; s++) {
+				System.out.print(spielfeld[z][s]);
 			}
 			System.out.println();
 		}
-		System.out.println("-----------------");
+		System.out.println("---------");
 	}
+	
+	public static String[][] spielfeldAnlegen(int zeilen, int spalten, boolean mienenAnlegen) {
+		String[][] mineField = new String[zeilen][spalten];
+		for (int z = 0; z < zeilen; z++) {
+			for (int s = 0; s < spalten; s++) {
+				mineField[z][s] = "[ ]";
+			}
+		}
+		if (mienenAnlegen) {
+			mineField[(zeilen-1)][0] = "[x]";
+			mineField[(zeilen-1)][(spalten-1)] = "[x]";
+			mineField[0][(spalten-1)] = "[x]";
+			mines = 3;
+		}
+		return mineField;
+	}
+	
+	
 }
