0c3c33193897b3d8dbbbe4434e85a1bbf585ab9f
OstapFilipenko <filipenkos_13@mail.ru>
2018-05-14 14:37:00 +0200

    mineSweeper v.2

--- Game.java
+++ Game.java
@@ -1,9 +1,27 @@
 
 public class Game {
-
+	static int _zeilen =  3;
+	static int _spalten = 3;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[][] feld = mineField(_zeilen,_spalten);
+		spielfeldAnzeigen(feld, _zeilen, _spalten);
+		
 	}
-
+	public static String[][] mineField(int Zeilen, int Spalten){
+		String[][] mineFeld  = new String[Zeilen][Spalten];
+		for(int i = 0; i<mineFeld.length;i++) {
+			for(int q = 0; q<mineFeld.length;q++) {
+				mineFeld[i][q] = "[ ]";
+			}
+		}
+		return mineFeld;
+	}
+	public static void spielfeldAnzeigen(String[][] feld, int zeilen, int spalten) {
+		for(int i = 0; i<zeilen;i++) {
+			for(int q = 0;q<spalten;q++) {
+				System.out.print(feld[i][q]);
+			}
+		System.out.println();
+		}
+	}
 }
